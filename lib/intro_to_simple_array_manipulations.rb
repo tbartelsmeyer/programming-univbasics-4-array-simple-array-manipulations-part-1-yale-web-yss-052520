845b230183daa64e1c97a4b6d34e4ad9cfd359e9
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-17 05:07:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,9 +10,6 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array,num2del)
-  2.times do
-    output[num2del - 1] = array.pop
-    num2del -= 1
-  end
+def pop_with_args(array)
+  array.pop(2)
 end
